e6871a2d5c88813324c1e8570dec32f184bc4384
Mdhlimon <mdhlimonmia@gmail.com>
2023-04-19 12:50:23 +0600

    Responsive done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import {Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const Header = () => {
+    const [open, setOpen] = useState(false);
+    console.log(open)
     return (
        <div className='header'>
          <h3>Jobs Portal</h3>
-         <nav>
+
+         <div onClick={()=> setOpen(!open)}>
+            <span>
+               {
+                  open === true ?
+                  <XMarkIcon className='bar-icon'></XMarkIcon>
+                  :<Bars3Icon className='bar-icon'></Bars3Icon>
+               }
+            </span>
+         </div>
+         <nav className={`nav-bar ${open ? 'open': 'close'}`}>
             <Link to="/">Home</Link>
             <Link to="/statistics">Statistics</Link>
             <Link to="/applied">Applied Jobs</Link>
